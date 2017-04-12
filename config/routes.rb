31a64097d3ebad243919e99f61def7c835d9c8aa
Paul <paucho1437@yahoo.com>
2017-04-12 17:27:32 -0500

    user profile implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
   get 'welcome/index'
-
   get 'welcome/about'
   root 'welcome#index'
 end
